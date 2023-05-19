6b955660d914979229b146b1de3aa74d9f9d6b0a
김익중 <rockij83@nate.com>
2023-05-19 15:55:45 +0900

    230519

--- guide/resources/js/project.js
+++ guide/resources/js/project.js
@@ -1,12 +1,17 @@
 let project = [
   {
-    dep2: 'Default Accordion',
-    dep3: 'Default Accordion',
-    dep4: 'Default Accordion',
+    dep2: 'text11',
+    dep3: 'text12',
+    dep4: 'text13',
   },
   {
-    dep2: 'Default Accordion',
-    dep3: 'Default Accordion',
-    dep4: 'Default Accordion',
+    dep2: 'text21',
+    dep3: 'text22',
+    dep4: 'text23',
+  },
+  {
+    dep2: 'text31',
+    dep3: 'text32',
+    dep4: 'text33',
   },
 ]
